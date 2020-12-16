503d6f0a9cab847a8c628fbbd63212165c91e960
amrullahdev <52965747+amrullahdev@users.noreply.github.com>
2020-12-17 00:28:12 +0700

    initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,17 @@
+<template>
+	<q-page>
+		<div class="q-mx-md">
+			<h5>
+				<q-icon name="event" />
+
+				Help
+			</h5>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga dolor nisi cumque dolorem nemo dolore nostrum sint excepturi, atque similique numquam iure? Fuga cum nobis quisquam eveniet optio, fugit.
+		</div>
+	</q-page>
+</template>
+<script>
+	export default{
+
+	}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <!-- <q-page class="flex flex-center"> -->
+  <q-page >
+  	<q-input
+        filled
+        square
+        bordered
+        class="q-px-md q-pt-md"
+        bg-color="white"
+        bottom-slots
+        v-model="newTask"
+        placeholder="Add Task"
+        maxlength="12"
+        :dense="dense"
+        @keyup.enter="tambahTask"
+        >
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" />
+        </template>
+      </q-input>
+  	<q-list v-for="(todo,index) in todos" :key="todo.title">
+	  	 <q-item
+			  	 @click="todo.status = !todo.status"
+			  	 clickable
+			  	 v-ripple
+			  	 :class="{'done bg-blue-1' : todo.status}"
+			  	 >
+	        <q-item-section side top>
+	          <q-checkbox
+		          v-model="todo.status"
+		          class="no-pointer-events" />
+	        </q-item-section>
+	        <q-item-section >
+	          <q-item-label>
+	            {{todo.title}}
+	          </q-item-label>
+	        </q-item-section>
+	        <q-item-section
+	        	@click.stop="removeTodo(index)"
+		        side
+		        v-if="todo.status" >
+	        	<q-btn
+		        	flat
+		        	round
+		        	color="primary"
+		        	icon="delete"/>
+	        </q-item-section>
+	      </q-item>
+	      <q-separator />
+      </q-list>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+  	return{
+  		newTask:'',
+  		dense:false,
+  		todos:[
+        // {
+        //   title:'Membeli Baju',
+        //   status:false
+        // },
+        // {
+        //   title:'Membeli Sepatu',
+        //   status:false
+        // },
+        // {
+        //   title:'Membeli Kue',
+        //   status:false
+        // }
+      ],
+  	}
+  },
+ methods:{
+ 	removeTodo(index){
+ 		this.todos.splice(index, 1)
+ 	},
+ 	tambahTask(){
+ 		this.todos.push({
+ 			title:this.newTask,
+ 			status:false,
+ 		})
+ 		this.newTask=''
+ 	},
+ }
+}
+</script>
+
+<style lang="scss">
+	.done{
+		.q-item__label{
+			text-decoration: line-through;
+			color:#bbb;
+		}
+
+	}
+</style>
