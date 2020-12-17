576c213aea714ba17e1d8ac9f96615c151bc14fc
amrullahdev <52965747+amrullahdev@users.noreply.github.com>
2020-12-17 09:21:12 +0700

    menghapus notify pada config{}

--- quasar.conf.js
+++ quasar.conf.js
@@ -77,7 +77,7 @@ module.exports = function (/* ctx */) {
     framework: {
       iconSet: 'material-icons', // Quasar icon set
       lang: 'en-us', // Quasar language pack
-      config: {Notify},
+      config: {},
 
       // Possible values for "importStrategy":
       // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
